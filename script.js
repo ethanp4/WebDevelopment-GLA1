b87f588802bab06153d691afeca1c9e0c37ffd3f
ethanp4 <ayyyuuw@gmail.com>
2024-01-23 19:36:43 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var body = document.getElementsByTagName("body")[0]
+var h1 = document.createElement("h1")
+h1.innerHTML = "Ethan"
+h1.style.color = "gray"
+
+var input1 = document.createElement("input")
+var input2 = document.createElement("input")
+body.appendChild(h1)
+body.appendChild(input1)
+body.appendChild(input2)
+
+var button = document.createElement("button")
+button.textContent = "text"
+body.appendChild(button)
+
+
+var resultDiv = document.createElement("div")
+body.appendChild(resultDiv)
+
+
+button.onclick = () => {
+    if (isNaN(input1.value) || isNaN(input2.value)) {
+        resultDiv.innerHTML = "Please enter a number"
+        resultDiv.style.color = "red"
+    } else {
+        resultDiv.innerHTML = parseInt(input1.value) + parseInt(input2.value)
+        resultDiv.style.color = "black"
+    }
+}
+
+
+
+
+
+
