f510d28cdc92cbc37ede7d45991c4fb8d629b39b
ethanp4 <ayyyuuw@gmail.com>
2024-01-23 20:10:39 -0700

    made better

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
+//create elements
 var body = document.getElementsByTagName("body")[0]
+var main = document.createElement("main")
 var h1 = document.createElement("h1")
+var input1 = document.createElement("input")
+var input2 = document.createElement("input")
+var button = document.createElement("button")
+var resultDiv = document.createElement("div")
+
+//set elements attributes
+main.style.fontFamily = "Arial, Helvetica, sans-serif"
+main.style.marginLeft = "40%"
+main.style.marginTop = "10%"
+
 h1.innerHTML = "Ethan"
 h1.style.color = "gray"
 
-var input1 = document.createElement("input")
-var input2 = document.createElement("input")
-body.appendChild(h1)
-body.appendChild(input1)
-body.appendChild(input2)
+button.textContent = "Add"
 
-var button = document.createElement("button")
-button.textContent = "text"
-body.appendChild(button)
-
-var resultDiv = document.createElement("div")
-body.appendChild(resultDiv)
-
-//function for button onclick
 button.onclick = () => {
-    if (isNaN(input1.value) || isNaN(input2.value)) {
+    try {
+        if (isNaN(parseInt(input1.value) + parseInt(input2.value))) {
+            throw "not a number"
+        }
+        resultDiv.innerHTML = parseInt(input1.value) + parseInt(input2.value)
+        input1.style.border = "1px solid black"
+        input2.style.border = "1px solid black"
+
+        resultDiv.style.color = "black"
+    } catch {
         resultDiv.innerHTML = "Please enter a number"
         resultDiv.style.color = "red"
-    } else {
-        resultDiv.innerHTML = parseInt(input1.value) + parseInt(input2.value)
-        resultDiv.style.color = "black"
+        input1.style.border = "1px solid red"
+        input2.style.border = "1px solid red"
     }
 }
 
-
-
-
-
+//append elements
+body.appendChild(main)
+main.appendChild(h1)
+main.appendChild(input1)
+main.appendChild(input2)
+main.appendChild(button)
+main.appendChild(resultDiv)
 
